5208b035c81741c51e6f1e2d58c4adfaf9c10589
Yukino Shiratama <npfjcg@live.cn>
2018-12-09 00:29:58 +0800

    Different error messages.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time, requests, json, urllib, getpass, os
 url_code = 'http://tcpe.upc.edu.cn:8086/Common/GetValiCode/'
 url_login = 'http://tcpe.upc.edu.cn:8086/Account/DoLogin'
 url_get_result = 'http://tcpe.upc.edu.cn:8086/Student/GetStuExpeDetail'
-ua = 'Molella/5.0 (YJSNPinux 114.51,KMR OS 19.1) RingoWebKit/810.0 (KHTML, like Gecko) Chrome/49.0.2623.13 Safari/537.36'
+ua = 'Molella/5.0 (YJSNPinux 114.51,KMR OS 19.1)'
 header = {"User-Agent": ua, 'Host': 'tcpe.upc.edu.cn:8086'}
 
 login_session = requests.session()
@@ -20,7 +20,7 @@ useraccount = input('请输入你的学号(如:1701010101):')
 password = getpass.getpass(prompt='请输入你的密码（你的输入有可能没有显示，这是正常的现象）:')
 usertype = 'Student'
 course_id = input(
-    '请输入你的课程编号，\n大学物理实验(2-1)请输入1\n大学物理实验(2-2)请输入2\n大学物理实验(小学期)请输入3 :')
+    '请输入你的课程编号，\n大学物理实验(2-1),请输入1\n大学物理实验(2-2),请输入2\n大学物理实验(小学期),请输入3 :')
 valicode = input('请输入与程序同一目录下的 验证码.png 显示的验证码:')
 login_session.get(url_login, headers=header)
 post_data = {
@@ -32,11 +32,13 @@ post_data = {
 }
 data = login_session.post(url_login, data=post_data, headers=header)
 # 登录
-
 if data.text != 'Ok':
-    print('出错了，你的登录信息可能有误!')
-    # 登录信息错误
-
+    if data.text == 'ValidateErr':
+        print('验证码错误')
+    elif data.text == 'EmptyErr':
+        print('输入的用户名或者密码为空')
+    elif data.text == 'AccPwdErr':
+        print('用户名或密码错误')
 else:
     result = requests.get(
         url_get_result, headers=header, cookies=getcookies.cookies.get_dict())
